db504a2d070f7171477c7304da4aff694a37af26
AlawiStore24 <159411067+AlawiStore24@users.noreply.github.com>
2024-02-10 18:19:52 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+Green="\e[92;1m"
+RED="\033[31m"
+YELLOW="\033[33m"
+BLUE="\033[36m"
+FONT="\033[0m"
+GREENBG="\033[42;37m"
+REDBG="\033[41;37m"
+OK="${Green}--->${FONT}"
+ERROR="${RED}[ERROR]${FONT}"
+GRAY="\e[1;30m"
+NC='\e[0m'
+red='\e[1;31m'
+green='\e[0;32m'
+
+
+#Instal pointing domain
+function pointing(){
+    clear
+    print_install "Memasang Packet Ponting Domain ke Cloudflare..."
+    wget https://raw.githubusercontent.com/AlawiStore24/repoindo/main/pointing.zip
+    unzip pointing.zip
+    chmod +x pointing/*
+    mv pointing/* /usr/local/sbin
+    rm -rf pointing
+    rm -rf pointing.zip
+}
+
+echo -e "system akan reboot setelah 3 detik"
+sleep 3
+reboot
