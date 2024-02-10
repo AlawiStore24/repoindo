ccbf9ced6af603f63777093e1cd01e4aefe5c023
AlawiStore24 <159411067+AlawiStore24@users.noreply.github.com>
2024-02-10 16:52:59 +0700

    Update pointing.sh

--- pointing.sh
+++ pointing.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+Green="\e[92;1m"
+RED="\033[31m"
+YELLOW="\033[33m"
 BLUE="\033[36m"
-
+FONT="\033[0m"
+GREENBG="\033[42;37m"
+REDBG="\033[41;37m"
+OK="${Green}--->${FONT}"
+ERROR="${RED}[ERROR]${FONT}"
+GRAY="\e[1;30m"
+NC='\e[0m'
+red='\e[1;31m'
+green='\e[0;32m'
 #pointing domain ke cloudflare
 apt install jq curl -y
 clear
